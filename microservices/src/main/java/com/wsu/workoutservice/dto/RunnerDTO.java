0899796a2adb5c3abb093ae1005643f2f5525693
Chase Ennis <ennis.18@wright.edu>
2024-12-03 14:03:04 -0500

    here

---
+++ microservices/src/main/java/com/wsu/workoutservice/dto/RunnerDTO.java
@@ -0,0 +1,63 @@
+
+
+package com.wsu.workoutservice.dto;
+
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class RunnerDTO {
+
+    
+
+    private Integer id; 
+
+    
+    @NotBlank(message = "Email must not be null or blank")
+    @Size(max = 25)
+    private String email;
+
+    
+    @NotBlank(message = "First Name must not be null or blank")
+    @Size(max = 25)
+    private String firstName;
+
+
+    @NotBlank(message = "Last Name must not be null or blank")
+    @Size(max = 50)
+    private String lastName;
+    
+   
+    @NotBlank(message = "Gender must not be null or blank")
+    @Size(max=10)
+    private String gender;
+
+    
+    @NotNull(message = "Height must not be null or blank")
+    @Min(value = 0, message = "height must be greater than 0")
+    private BigDecimal height;
+
+    
+    @NotNull(message = "Age must not be null or blank")
+    @Min(value = 0, message = "Age must be greater than 0")
+    private Integer age;
+
+    @NotNull(message = "Height must not be null or blank")
+    @Min(value = 0, message = "height must be greater than 0")
+    private BigDecimal weight;
+    
+}
---
+++ microservices/src/main/java/com/wsu/workoutservice/model/Runner.java
@@ -0,0 +1,52 @@
+package com.wsu.workoutservice.model;
+
+import java.math.BigDecimal;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
+@Table(name = "runner")
+public class Runner {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "runner_id")
+    private Integer id;
+
+    @Column(name= "email")
+    private String email;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "gender")
+    private String gender;
+
+    @Column(name = "height")
+    private BigDecimal height;
+
+    @Column(name = "age")
+    private Integer age;
+
+    @Column(name = "weight")
+    private BigDecimal weight;
+
+}
